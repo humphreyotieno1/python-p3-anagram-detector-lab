76d0e854472103c89e49ee5e74e7bb8313c6c1ac
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-03-09 23:18:39 +0300

    Anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, listen):
+        self.listen = listen
+        
+        
+    def is_anagram(self, word):
+        return sorted(word.lower()) == sorted(self.listen.lower())
+    
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word)]
+    
+anagram = Anagram("listen")
+words = ["enlist", "silent", "google", "listen"]
+print(anagram.match(words))
